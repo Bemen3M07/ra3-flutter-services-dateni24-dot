3b1b894e7c9ab1b39796b10c4928c70157dcfe1a
Daniel <dateni24@bemen3.cat>
2026-03-02 18:53:17 +0100

    test y pubspec

--- lib/car_data_service.dart
+++ lib/car_data_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:ra3-flutter-services-dateni24-dot/car.dart';
+import 'car.dart';
 
 class CarDataService {
   final String apiKey = 'e45d51ce02mshd4e4d6f2062715ep1bb9e7jsnb5f1b684bd95';
@@ -18,51 +18,6 @@ class CarDataService {
       return carsFromJson(response.body);
     } else {
       throw Exception('Failed to load cars');
-    }
-  }
-
-  Future<List<dynamic>> getTypes() async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/types'),
-      headers: {
-        'x-rapidapi-key': apiKey,
-        'x-rapidapi-host': 'car-data.p.rapidapi.com',
-      },
-    );
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Failed to load types');
-    }
-  }
-
-  Future<List<dynamic>> getMakes() async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/makes'),
-      headers: {
-        'x-rapidapi-key': apiKey,
-        'x-rapidapi-host': 'car-data.p.rapidapi.com',
-      },
-    );
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Failed to load makes');
-    }
-  }
-
-  Future<List<dynamic>> getYears() async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/years'),
-      headers: {
-        'x-rapidapi-key': apiKey,
-        'x-rapidapi-host': 'car-data.p.rapidapi.com',
-      },
-    );
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Failed to load years');
     }
   }
 }
--- test/car_data_service_test.dart
+++ test/car_data_service_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ra3-flutter-services-dateni24-dot/car_data_service.dart';
-import 'package:ra3-flutter-services-dateni24-dot/car.dart';
+import '../lib/car_data_service.dart';
+import '../lib/car.dart';
 
 void main() {
   test('getCars returns Car objects', () async {
@@ -8,23 +8,5 @@ void main() {
     final cars = await service.getCars();
     expect(cars, isNotEmpty);
     expect(cars.first, isA<Car>());
-  });
-
-  test('getTypes returns data', () async {
-    final service = CarDataService();
-    final types = await service.getTypes();
-    expect(types, isNotEmpty);
-  });
-
-  test('getMakes returns data', () async {
-    final service = CarDataService();
-    final makes = await service.getMakes();
-    expect(makes, isNotEmpty);
-  });
-
-  test('getYears returns data', () async {
-    final service = CarDataService();
-    final years = await service.getYears();
-    expect(years, isNotEmpty);
   });
 }
